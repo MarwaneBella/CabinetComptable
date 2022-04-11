ba7588cda272db85265e5c4d4382888a1d44d8bc
MarwaneBella <bellaworks00@gmail.com>
2022-04-11 15:39:18 +0000

    first commit

---
+++ src/main/java/com/example/cabinetcomptable/CabinetComptableApplication.java
@@ -0,0 +1,13 @@
+package com.example.cabinetcomptable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetComptableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabinetComptableApplication.class, args);
+    }
+
+}
