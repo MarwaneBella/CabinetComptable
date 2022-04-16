ea43a93989c1bb277e0b82d7bf1fc0c9f823c5ec
MarwaneBella <bellaworks00@gmail.com>
2022-04-16 17:21:01 +0000

    add comptable class

--- src/main/java/com/example/cabinetcomptable/controllers/ComptableController.java
+++ src/main/java/com/example/cabinetcomptable/controllers/ComptableController.java
@@ -1,10 +1,7 @@
 package com.example.cabinetcomptable.controllers;
 
 import com.example.cabinetcomptable.entities.Comptable;
-import com.example.cabinetcomptable.repositories.ComptableRepository;
 import com.example.cabinetcomptable.services.ComptableService;
-import com.example.cabinetcomptable.services.impl.ComptableServiceImpl;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
