a8098533d36713d46354ed36255e0f6f5b347486
MarwaneBella <bellaworks00@gmail.com>
2022-04-19 16:31:01 +0000

    //list-client

--- src/main/java/com/example/cabinetcomptable/controllers/ClientController.java
+++ src/main/java/com/example/cabinetcomptable/controllers/ClientController.java
@@ -2,8 +2,7 @@ package com.example.cabinetcomptable.controllers;
 
 
 import com.example.cabinetcomptable.entities.Client;
-import com.example.cabinetcomptable.repositories.ClientRepository;
-import com.example.cabinetcomptable.services.ClientService;
+
 import com.example.cabinetcomptable.services.impl.ClientServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,15 +16,12 @@ public class ClientController {
 
     @Autowired
     private ClientServiceImpl clientService;
-    //
-    @Autowired
-    private ClientRepository clientRepository;
+
 
     //
     @GetMapping("/listClient")
     public List<Client> loginComptable(){
 
         return clientService.getAllClient();
-      //  return clientRepository.findAll();
     }
 }
